596d379b8ae7d6d374106dae9e91694f69fe0e7b
Melvin Giovanni Domingo <domingotaiga@gmail.com>
2024-04-06 11:51:53 +0800

    new

--- script.js
+++ script.js
@@ -71,11 +71,18 @@ const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 
-toggleBtn.onclick = function () {
+toggleBtn.onclick = function (event) {
+  event.stopPropagation(); //
   dropDownMenu.classList.toggle("open");
   const isOpen = dropDownMenu.classList.contains("open");
-
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+document.onclick = function (event) {
+  if (dropDownMenu.classList.contains("open")) {
+    dropDownMenu.classList.remove("open");
+    toggleBtnIcon.classList = "fa-solid fa-bars";
+  }
 };
 
 ///messagw
